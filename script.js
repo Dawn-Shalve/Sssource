fb86b9be7355a8c2f93c936b64060a7ad7db64e4
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-08-30 16:05:13 -0700

    yes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-var yes = 3
+//homepage toggle code
+function myFunction() {
+    var x = document.getElementById("homepage");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
