bd900d7abc881a43423b6d620b42eec0104a780e
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-09-05 14:17:00 -0700

    BIG OVERHAUL

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//homepage toggle code
-function myFunction() {
-    var x = document.getElementById("homepage");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+function articleuno(){
+  
+}
