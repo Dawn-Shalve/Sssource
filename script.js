797f92b52108c5e70eb9345497b450b3761e72cb
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-09-27 09:45:27 -0700

    ARTICLE CHANGING IS DONEEEEE WOOOOOO

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function articleuno(){
-  
+function article(){
+    document.getElementById("homepage").style.display ="none";
+    document.getElementById("articletemp").style.display="contents"
+}
+function homepage(){
+    document.getElementById("homepage").style.display ="contents";
+    document.getElementById("articletemp").style.display="none"
 }
