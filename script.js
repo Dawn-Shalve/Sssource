4c55365f7c976c457428def155081c8633d9cb08
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-09-27 14:31:09 -0700

    ARTICLE BROWSER IS DONE WHY THE HELL WAS THE ARTICLE ELEMENT ONLY ON ROW 1?!?!?!

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function article(){
     document.getElementById("homepage").style.display ="none";
-    document.getElementById("articletemp").style.display="contents"
+    document.getElementById("articlebrowser").style.display="none";
+    document.getElementById("articletemp").style.display="contents";
 }
 function homepage(){
     document.getElementById("homepage").style.display ="contents";
+    document.getElementById("articletemp").style.display="none";
+    document.getElementById("articlebrowser").style.display="none"
+    document.getElementById("homebutton").className = "active";
+    document.getElementById("articlebutton").className = "none";
+}
+function articlebrowser(){
+    document.getElementById("homepage").style.display ="none";
     document.getElementById("articletemp").style.display="none"
+    document.getElementById("articlebrowser").style.display="content"
+    document.getElementById("articlebutton").className = "active";
+    document.getElementById("homebutton").className = "none"
+}
+function disableallsites(){
+    document.getElementById("homepage").style.display="none";
+    document.getElementById("articletemp").style.display="none";
+    document.getElementById("articlebrowser").style.display="none";
+    /// Make Sure to add EVERY site template to this function \\\
 }
