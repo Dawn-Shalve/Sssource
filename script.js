f66cbdd7e139c01a3a1ea1cf2a307ca0da634aa0
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2023-09-28 09:11:42 -0700

    broken release

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function article(){
     document.getElementById("homepage").style.display ="none";
     document.getElementById("articlebrowser").style.display="none";
     document.getElementById("articletemp").style.display="contents";
+    disableallsites()
+
 }
 
 function homepage(){
@@ -10,21 +12,32 @@ function homepage(){
     document.getElementById("articlebrowser").style.display="none"
     document.getElementById("homebutton").className = "active";
     document.getElementById("articlebutton").className = "none";
+    disableallsites()
+    document.getElementById("homeppppppage")
 }
 function articlebrowser(){
     document.getElementById("homepage").style.display ="none";
     document.getElementById("articletemp").style.display="none"
     document.getElementById("articlebrowser").style.display="content"
     document.getElementById("articlebutton").className = "active";
-    document.getElementById("homebutton").className = "none"
+    document.getElementById("homebutton").className = "none";
+    document.getElementById("about").style.display ="none";
+    document.getElementById("about").className = "none";
+    disableallsites()
+    document.getElementById("articlebrowser").style.display="content";
 }
 function disableallsites(){
     document.getElementById("homepage").style.display="none";
     document.getElementById("articletemp").style.display="none";
     document.getElementById("articlebrowser").style.display="none";
+    document.getElementById("about").style.display ="none";
     /// Make Sure to add EVERY site template to this function \\\
 }
 function Start(){
     disableallsites();
     homepage();
+    debugmode();
+}
+function debugmode(){
+    disableallsites();
 }
